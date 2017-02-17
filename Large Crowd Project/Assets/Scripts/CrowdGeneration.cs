26310fdc1b7d61fe20e5f5e3ff92b87a4b1af333
Lee Kempton <lee.kempton@students.plymouth.ac.uk>
2017-02-17 15:00:53 +0000

    Added the ability to generate crowds by using rows and columns with a distance offset between them all. Test at 100*100 with a 1.5 offset; takes a long time to generate.

--- Large Crowd Project/Assets/Scripts/CrowdGeneration.cs
+++ Large Crowd Project/Assets/Scripts/CrowdGeneration.cs
@@ -3,13 +3,40 @@ using System.Collections;
 
 public class CrowdGeneration : MonoBehaviour {
 
+    [SerializeField]
+    private int _rows, _columns;
+
+    [SerializeField]
+    private float _offset = 0.5f;
+
+    [SerializeField]
+    private GameObject _crowdObject;
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+    {
+        GenerateCrowd();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    void GenerateCrowd()
+    {
+        var _transform = gameObject.transform;
+        int _objCount = 0;
+
+        var _objCollider = _crowdObject.GetComponent<CapsuleCollider>().height;
+
+        for (int i = 0; i < _columns; i++)
+        {
+            for (int j = 0; j < _rows; j++)
+            {
+                var _objPos = new Vector3(_transform.position.x + i * _offset, transform.position.y + (_objCollider / 2), _transform.position.z + j * _offset);
+
+                var _obj = Instantiate(_crowdObject, _objPos, _transform.rotation, _transform);
+
+                _objCount++;
+                print(_objCount);
+                
+            }
+        }
+
+    }
 }
