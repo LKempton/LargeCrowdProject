dbe6c2db65d2207bb975e1fdea94f11dbed10b0d
pharezvitalis <pharez.vitalis@students.plymouth.ac.uk>
2017-02-22 11:56:56 +0000

    encapsulated class inside of a namespace

--- Large Crowd Project/Assets/Scripts/CrowdGeneration.cs
+++ Large Crowd Project/Assets/Scripts/CrowdGeneration.cs
@@ -1,60 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-public class CrowdGeneration : MonoBehaviour {
-
-    [SerializeField]
-    private int _rows, _columns;
-
-    [SerializeField]
-    private float _minOffset, _maxOffset, _tiltAmount, _tiltStart;
-
-    [SerializeField]
-    private GameObject _crowdObject;
-	// Use this for initialization
-	void Start () 
+namespace CrowdAI
+{
+    public class CrowdGeneration : MonoBehaviour
     {
-        GenerateCrowd();
-	}
 
-    void GenerateCrowd()
-    {
-        // Diagnostic tool to test how long a method takes to run.
-        var watch = System.Diagnostics.Stopwatch.StartNew();
+        [SerializeField]
+        private int _rows, _columns;
 
-        var _transform = gameObject.transform;
-        int _objCount = 0;
+        [SerializeField]
+        private float _minOffset, _maxOffset, _tiltAmount, _tiltStart;
 
-        var _objCollider = _crowdObject.GetComponent<MeshRenderer>().bounds.size.y * _transform.localScale.y ;
-
-        // Run through rows and columns and generate objects as needed. 
-        // Make them a child of the source object.
-        
-
-        for (int i = 0; i < _columns; i++)
+        [SerializeField]
+        private GameObject _crowdObject;
+        // Use this for initialization
+        void Start()
         {
-            for (int j = 0; j < _rows; j++)
-            {
-                var _offset = Random.Range(_minOffset, _maxOffset);
-                var _objPos = new Vector3(_transform.position.x + i * _offset, transform.position.y + (_objCollider / 2) + _tiltStart, _transform.position.z + j * _offset);
-
-                var _obj = Instantiate(_crowdObject, _objPos, _transform.rotation, _transform);
-
-                _objCount++;
-            }
-
-            if (_tiltAmount != 0)
-            {
-                _tiltStart = _tiltStart + _tiltAmount;
-            }
-            
+            GenerateCrowd();
         }
 
-        watch.Stop();
-        var _elapsedTime = watch.ElapsedMilliseconds;
+        void GenerateCrowd()
+        {
+            // Diagnostic tool to test how long a method takes to run.
+            var watch = System.Diagnostics.Stopwatch.StartNew();
 
-        Debug.Log(System.String.Format("Generated {0} objects in {1} milliseconds.", _objCount, _elapsedTime));
-       
+            var _transform = gameObject.transform;
+            int _objCount = 0;
 
+            var _objCollider = _crowdObject.GetComponent<MeshRenderer>().bounds.size.y * _transform.localScale.y;
+
+            // Run through rows and columns and generate objects as needed. 
+            // Make them a child of the source object.
+
+
+            for (int i = 0; i < _columns; i++)
+            {
+                for (int j = 0; j < _rows; j++)
+                {
+                    var _offset = Random.Range(_minOffset, _maxOffset);
+                    var _objPos = new Vector3(_transform.position.x + i * _offset, transform.position.y + (_objCollider / 2) + _tiltStart, _transform.position.z + j * _offset);
+
+                    var _obj = Instantiate(_crowdObject, _objPos, _transform.rotation, _transform);
+
+                    _objCount++;
+                }
+
+                if (_tiltAmount != 0)
+                {
+                    _tiltStart = _tiltStart + _tiltAmount;
+                }
+
+            }
+
+            watch.Stop();
+            var _elapsedTime = watch.ElapsedMilliseconds;
+
+            Debug.Log(System.String.Format("Generated {0} objects in {1} milliseconds.", _objCount, _elapsedTime));
+
+
+        }
     }
 }
