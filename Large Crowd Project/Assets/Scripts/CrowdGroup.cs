9fe73485f62ebddcf2f977962366f1a41d82f561
pvitalis <pvitalis>
2017-02-23 13:30:00 +0000

    Made an interface for each crowd member. Further works on the crowd group class. Began implementing a master controller class

--- Large Crowd Project/Assets/Scripts/CrowdGroup.cs
+++ Large Crowd Project/Assets/Scripts/CrowdGroup.cs
@@ -1,16 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System;
 
 namespace CrowdAI
 {
     public class CrowdGroup
     {
         private string _groupName;
-        private List<GameObject> _crowdMemebers;
+        private List<ICrowd> _crowdMemebers;
 
          public CrowdGroup(string groupName)
         {
-            _crowdMemebers = new List<GameObject>();
+            _crowdMemebers = new List<ICrowd>();
 
             _groupName = groupName;
         }
@@ -19,14 +20,40 @@ namespace CrowdAI
         {
             _groupName = groupName;
 
-            _crowdMemebers = new List<GameObject>();
+            _crowdMemebers = new List<ICrowd>();
 
             for (int i = 0; i < crowdMemebers.Length; i++)
             {
-                _crowdMemebers.Add(crowdMemebers[i]);
+                _crowdMemebers.Add(crowdMemebers[i].GetComponent<ICrowd>());
             }
 
         }
+
+        public void AddCrowdMember(GameObject crowdMember)
+        {
+            _crowdMemebers.Add(crowdMember.GetComponent<ICrowd>());
+        }
+
+        public void AddCrowdMember(ICrowd crowdMeber)
+        {
+            _crowdMemebers.Add(crowdMeber);
+        }
+
+        public void AddCrowdMember(object crowdMember)
+        {
+            //overloaded because Instansiate function returns an object
+
+            var _goCrowdMember = (GameObject)crowdMember;
+
+                _crowdMemebers.Add(_goCrowdMember.GetComponent<ICrowd>());
+            
+        }
+
+        public void ClearAll()
+        {
+            _crowdMemebers.Clear();
+        }
+
 
     }
 
---
+++ Large Crowd Project/Assets/Scripts/ICrowd.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CrowdAI
+{
+    public interface ICrowd
+    {
+        void SetState(string State);
+        string GetState();
+        void StartAnimations(float delay);
+        void StartAnimation();
+        void StopAnimation();
+        bool LoopAnimation { get; set; }
+       
+        // possibly use a way of using a mix of global and local animations
+    }
+
+  
+}
