945a7152a37c9bd6d24312fba6b46dc29634501d
pharezvitalis <pharez.vitalis@students.plymouth.ac.uk>
2017-02-23 12:41:57 +0000

    Began implementing a system that allows users to group crowd members into different categories

---
+++ Large Crowd Project/Assets/Scripts/CrowdGroup.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace CrowdAI
+{
+    public class CrowdGroup
+    {
+        private string _groupName;
+        private List<GameObject> _crowdMemebers;
+
+         public CrowdGroup(string groupName)
+        {
+            _crowdMemebers = new List<GameObject>();
+
+            _groupName = groupName;
+        }
+
+        public CrowdGroup(string groupName, GameObject[] crowdMemebers)
+        {
+            _groupName = groupName;
+
+            _crowdMemebers = new List<GameObject>();
+
+            for (int i = 0; i < crowdMemebers.Length; i++)
+            {
+                _crowdMemebers.Add(crowdMemebers[i]);
+            }
+
+        }
+
+    }
+
+    
+}
