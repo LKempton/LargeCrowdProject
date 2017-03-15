b73ab5c75e2102e1bdcf3265aa20d3cb25c79e9e
pvitalis <pvitalis>
2017-03-15 12:50:32 +0000

    made a static class that generates empty placeholder objects that are the positions of where the different crowd members

---
+++ Large Crowd Project/Assets/Scripts/CrowdGen.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CrowdAI
+{
+    public static class CrowdGen
+    {
+
+        public static void GenCrowdCicle()
+        {
+
+        }
+
+        public static void GenCrowdSquare(int columns, int rows, GameObject parent, Vector3 size, bool randomDist, float yOffset, float minGapSize, float maxGapSize)
+        {
+            var _placeHolder = new GameObject();
+            _placeHolder.AddComponent<GenericCrowdMember>();
+            var _parentPos = parent.transform.position;
+
+            
+
+            for (int i = 0; i < columns; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    var offset = size;
+                    offset.x += Random.Range(minGapSize, maxGapSize);
+                    
+                    offset.z = Random.Range(minGapSize, maxGapSize);
+                    var _newPos = _parentPos + new Vector3(i*offset.x,yOffset, offset.z*j );
+                }
+            }
+
+        }
+
+
+        public static Vector3 GetObjectBounds(GameObject gO, bool is3D)
+        {
+            var _outBounds = new Vector3();
+
+            if (is3D)
+            {
+                var _rend = gO.GetComponents<MeshRenderer>();
+
+                if (_rend.Length < 1)
+                {
+                    _rend = gO.GetComponentsInChildren<MeshRenderer>();
+                }
+
+                for (int i = 0; i < _rend.Length; i++)
+                {
+                    var _newBounds = _rend[i].bounds.extents;
+
+                    if (_outBounds.x < _newBounds.x)
+                    {
+                        _outBounds.x = _newBounds.x;
+                    }
+
+                    if (_outBounds.y < _newBounds.y)
+                    {
+                        _outBounds.y = _newBounds.y;
+                    }
+
+                    if (_outBounds.z < _newBounds.z)
+                    {
+                        _outBounds.z = _newBounds.z;
+                    }
+                }
+            }
+            else
+            {
+                var _spriteRend = gO.GetComponents<SpriteRenderer>();
+
+                if (_spriteRend == null)
+                {
+                    _spriteRend = gO.GetComponentsInChildren<SpriteRenderer>();
+                }
+
+                for (int i = 0; i < _spriteRend.Length; i++)
+                {
+                    var _newBounds = _spriteRend[i].bounds.extents;
+
+                    if (_outBounds.x < _newBounds.x)
+                    {
+                        _outBounds.x = _newBounds.x;
+                    }
+
+                    if (_outBounds.y < _newBounds.y)
+                    {
+                        _outBounds.y = _newBounds.y;
+                    }
+
+                    if (_outBounds.z < _newBounds.z)
+                    {
+                        _outBounds.z = _newBounds.z;
+                    }
+
+
+                }
+            }
+
+            _outBounds.x *= gO.transform.localScale.x * 2;
+            _outBounds.y *= gO.transform.localScale.y * 2;
+            _outBounds.z *= gO.transform.localScale.z * 2;
+
+            return _outBounds;
+
+        }
+    }
+
+}
