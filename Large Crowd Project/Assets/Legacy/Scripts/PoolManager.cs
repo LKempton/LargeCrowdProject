3e547d5ec1f289395a33431d321ea22cfe181615
RudiPrentice <rudiprentice@gmail.com>
2017-03-27 13:26:56 +0100

    Added LOD pool manager to crowd controller.

--- Large Crowd Project/Assets/Legacy/Scripts/PoolManager.cs
+++ Large Crowd Project/Assets/Legacy/Scripts/PoolManager.cs
@@ -7,13 +7,17 @@ namespace CrowdAI
 
     public class PoolManager : MonoBehaviour
     {
+        [SerializeField]
+        private int crowdNumber;
+        [SerializeField]
+        private int lodLayers;
 
         [SerializeField]
         private string[] objectNames;
         [SerializeField]
-        private GameObject[] pooledObjects;
+        private GameObject[] crowdLODObjects;
         [SerializeField]
-        private int[] pooledAmounts;
+        private int[] objectAmounts;
 
         private Hashtable mainPool = new Hashtable();
 
@@ -23,13 +27,22 @@ namespace CrowdAI
         {
             tempList = new List<GameObject>();
 
+            objectNames = new string[lodLayers];
+            crowdLODObjects = new GameObject[lodLayers];
+            objectAmounts = new int[lodLayers];
+
+            for (int i = 0; i < objectAmounts.Length; i++ )
+            {
+                objectAmounts[i] = crowdNumber;
+            }
+
             for (int i = 0; i < objectNames.Length; i++)
             {
                 List<GameObject> objList = new List<GameObject>();
 
-                for (int j = 0; j < pooledAmounts[i]; j++)
+                for (int j = 0; j < objectAmounts[i]; j++)
                 {
-                    GameObject obj = Instantiate(pooledObjects[i]);
+                    GameObject obj = Instantiate(crowdLODObjects[i]);
                     obj.transform.parent = transform;
                     objList.Add(obj);
                 }
---
+++ Large Crowd Project/Assets/Scripts/LODPoolManager.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CrowdAI
+{
+
+    public class LODPoolManager : MonoBehaviour
+    {
+        [SerializeField]
+        private int crowdNumber;
+        [SerializeField]
+        private int lodLayers;
+
+        [SerializeField]
+        private string[] objectNames;
+        [SerializeField]
+        private GameObject[] crowdLODObjects;
+        [SerializeField]
+        private int[] objectAmounts;
+
+        private Hashtable mainPool = new Hashtable();
+
+        private List<GameObject> tempList;
+
+        void Start()
+        {
+            tempList = new List<GameObject>();
+
+            objectNames = new string[lodLayers];
+            crowdLODObjects = new GameObject[lodLayers];
+            objectAmounts = new int[lodLayers];
+
+            for (int i = 0; i < objectAmounts.Length; i++)
+            {
+                objectAmounts[i] = crowdNumber;
+            }
+
+            for (int i = 0; i < objectNames.Length; i++)
+            {
+                List<GameObject> objList = new List<GameObject>();
+
+                for (int j = 0; j < objectAmounts[i]; j++)
+                {
+                    GameObject obj = Instantiate(crowdLODObjects[i]);
+                    obj.transform.parent = transform;
+                    objList.Add(obj);
+                }
+
+                mainPool.Add(objectNames[i], objList);
+            }
+        }
+
+        public GameObject GetPooledObject(string name)
+        {
+            if (mainPool.ContainsKey(name))
+            {
+                tempList = mainPool[name] as List<GameObject>;
+
+                for (int i = 0; i < tempList.Count; i++)
+                {
+                    if (tempList[i] != null)
+                    {
+                        if (!tempList[i].activeInHierarchy)
+                        {
+                            return tempList[i];
+                        }
+                    }
+                }
+                throw new System.Exception("There are no more inactive instances of the object you searched for in the pooler");
+            }
+
+            throw new System.Exception("There are no objects in the pooler with the name you searched for.");
+        }
+
+        public void ResetPool()
+        {
+            for (int i = 0; i < tempList.Count; i++)
+            {
+                if (tempList[i] != null)
+                {
+                    tempList[i].SetActive(false);
+                }
+            }
+        }
+    }
+}
