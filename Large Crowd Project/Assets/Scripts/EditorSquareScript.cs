08b35b8a741293f8a8d6cb70df3745dfbb38c450
lkempton <lkempton>
2017-03-27 15:33:43 +0100

    Added in a square that shows the bounds and can be customized to any rgb colour.

---
+++ Large Crowd Project/Assets/Scripts/EditorSquareScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorSquareScript : MonoBehaviour {
+
+    private Transform source;
+    private Transform bounds;
+
+    [SerializeField]
+    private Color selectedColour;
+
+    void OnDrawGizmosSelected()
+    {
+        source = transform;
+        bounds = transform.GetChild(0).transform;
+
+        if (bounds != null && source != null)
+        {
+            Gizmos.color = selectedColour;
+
+            Gizmos.DrawLine(source.position, new Vector3(source.position.x, source.position.y, bounds.position.z));
+            Gizmos.DrawLine(source.position, new Vector3(bounds.position.x, source.position.y, source.position.z));
+
+            Gizmos.DrawLine(bounds.position, new Vector3(bounds.position.x, bounds.position.y, source.position.z));
+            Gizmos.DrawLine(bounds.position, new Vector3(source.position.x, bounds.position.y, bounds.position.z));
+        }
+    }
+}
