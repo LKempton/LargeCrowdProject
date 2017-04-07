9a90b3fd4da453a9a61a2e7f0b2a1e672729ec9f
Rudi Jay <rudi.prentice@students.plymouth.ac.uk>
2017-04-07 18:49:57 +0100

    Commented LODPoolManager script

--- Large Crowd Project/Assets/Scripts/LODPoolManager.cs
+++ Large Crowd Project/Assets/Scripts/LODPoolManager.cs
@@ -18,6 +18,13 @@ namespace CrowdAI
 
         private List<GameObject> _tempList;
 
+		/// <summary>
+		/// Instantiates and initialises pooled objects
+		/// </summary>
+		/// <param name="size">the total number of all crowd members</param>
+		/// <param name="layers">the number of levels of detail for the model of each crowd member</param>
+		/// <param name="objects">the array containing the crowd model objects to be pooled (one for each LOD for each crowd member type e.g. medium detail model for woman model on red team)</param>
+		/// <param name="objectNames">the array containing the names of each crowd model object to be pooled (one for each LOD for each crowd member type e.g. mediumDetailWomanRed</param>
         public LODPoolManager(int size, int layers, GameObject[] objects, string[] objectNames)
         {
             _crowdNumber = size;
@@ -52,6 +59,12 @@ namespace CrowdAI
             }
         }
 
+		/// <summary>
+		/// Searches the pooled objects to find the next available object to activate.
+		/// Returns if found, throws exception if not
+		/// </summary>
+		/// <returns>The next available pooled object to be activated</returns>
+		/// <param name="name">the name of the type of pooled object to look for</param>
         public GameObject GetPooledObject(string name)
         {
             if (_mainPool.ContainsKey(name))
@@ -74,6 +87,9 @@ namespace CrowdAI
             throw new System.Exception("There are no objects in the pooler with the name you searched for.");
         }
 
+		/// <summary>
+		/// Deactivates all pooled objects
+		/// </summary>
         public void ResetPool()
         {
             for (int i = 0; i < _tempList.Count; i++)
