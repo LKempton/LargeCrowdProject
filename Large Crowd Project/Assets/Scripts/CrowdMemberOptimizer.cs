b0fc6199faaed88d9b6e47d1beccbc7342ab256e
RudiPrentice <rudiprentice@gmail.com>
2017-05-04 18:28:23 +0100

    Added crowd members and camera to separate collision layers from other objects

--- Large Crowd Project/Assets/Scripts/CrowdMemberOptimizer.cs
+++ Large Crowd Project/Assets/Scripts/CrowdMemberOptimizer.cs
@@ -20,84 +20,18 @@ namespace CrowdAI
         [SerializeField]
         private Collider _collider;
 
-        //[SerializeField]
-        //private float _veryHighLODDistance;
-        //[SerializeField]
-        //private float _highLODDistance;
-        //[SerializeField]
-        //private float _midLODDistance;
-        //[SerializeField]
-        //private float _lowLODDistance;
-        //[SerializeField]
-        //private float _veryLowLODDistance;
-
-        //[SerializeField]
-        //private GameObject[] LODModels;
-
-        //private int currentLODNumber;
-
         void OnBecameVisible()
         {
-            _collider.enabled = true;
+            if (_dynamicCrowdModel)
+            {
+                _collider.enabled = true;
+            }
         }
 
         void OnBecameInvisible()
         {
             _collider.enabled = false;
         }
-
-        /// <summary>
-        /// Gets the distance of the crowdmember from the camera
-        /// </summary>
-        //private void UpdateLOD()
-        //{
-        //    distanceToCam = CalculateDistance();
-        //    Debug.Log(distanceToCam);
-
-        //    int newLODNumber = 0;
-
-        //    if (distanceToCam <= _veryHighLODDistance)
-        //    {
-        //        newLODNumber = 4;
-        //    }
-        //    else if (distanceToCam > _veryHighLODDistance && distanceToCam <= _highLODDistance)
-        //    {
-        //        newLODNumber = 3;
-        //    }
-        //    else if (distanceToCam > _highLODDistance && distanceToCam <= _midLODDistance)
-        //    {
-        //        newLODNumber = 2;
-        //    }
-        //    else if (distanceToCam > _midLODDistance && distanceToCam <= _lowLODDistance)
-        //    {
-        //        newLODNumber = 1;
-        //    }
-        //    else if (distanceToCam > _lowLODDistance)
-        //    {
-        //        newLODNumber = 0;
-        //    }
-
-        //    if (newLODNumber != currentLODNumber)
-        //    {
-        //        SetNewLOD(newLODNumber);
-        //    }
-        //}
-
-        //void OnBecameVisible()
-        //{
-        //    if (_dynamicCrowdModel)
-        //    {
-        //        InvokeRepeating("UpdateLOD", 0, _updateFrequency);
-        //    }
-        //}
-
-        //void OnBecameInvisible()
-        //{
-        //    if (_dynamicCrowdModel)
-        //    {
-        //        CancelInvoke("UpdateLOD");
-        //    }
-        //}
 
         //private float CalculateDistance()
         //{
---
+++ Large Crowd Project/Assets/Scripts/DynamicLODCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicLODCamera : MonoBehaviour {
+
+    [SerializeField]
+    private Collider veryLowLODCollider, lowLODCollider, midLODCollider, highLODCollider, veryHighLODCollider;
+
+    void Update()
+    {
+
+    }
+}
